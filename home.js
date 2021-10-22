ef7ff5651bac0a961b8a61e20cd7a953f068d6a5
Austin <82212804+AustinKojo@users.noreply.github.com>
2021-10-22 23:01:48 +0000

    Add files via upload

---
+++ home.js
@@ -0,0 +1,96 @@
+import React , {useState,useRef}from "react";
+import { View, Text, FlatList, Image } from "react-native";
+import{ Feather, FontAwesome, AntDesign, 
+    Ionicons, Foundation, FontAwesome5, MaterialIcons}
+from '@expo/vector-icons';
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const IMAGES = {
+    image1: require('../assets/bic6.png'),
+    image2: require('../assets/bic2.jpeg'),
+    image3: require('../assets/bic3.jpeg'),
+    image4: require('../assets/bic4.jpeg'),
+};
+
+
+export default function home({navigation}) {
+    const [images, setimages] = useState([
+        
+        {image: IMAGES.image1, id:'1',
+        name: "mountain bike", price:'$ 1700.00'
+    },
+        {image: IMAGES.image2, id:'2',
+        name: "mountain bike", price:'$ 1700.00'},
+        {image: IMAGES.image3, id:'3',
+        name: "mountain bike", price:'$ 1700.00'},
+        {image: IMAGES.image4, id:'4',name:"mountain bike",price:'$ 1700.00'}
+    ]);
+    return <View style={{backgroundColor:"white", flex:1, paddingTop:55, paddingHorizontal:20}}>
+        <View style={{flexDirection:"row", justifyContent:"space-between"}}>
+            <Feather name="menu" size={24} />
+            <FontAwesome name="motorcycle" size={24} />
+            <View style={{flexDirection:"row", justifyContent:"space-between", }}>
+                <AntDesign name="search1" size={24}  />
+                <Ionicons name="notifications-outline" size={24} />
+            </View> 
+        </View>
+        <View style={{flexDirection:"row", marginTop:20}}>
+            <Text style={{color:"grey",fontSize:15}}>
+                The World's  <Text style={{color:"orange",fontWeight:"600",fontSize:20}}>Best Bike</Text>
+            </Text>
+        </View>
+        <View>
+            <Text style={{fontWeight:"600", marginTop:20}}>Categories</Text>
+        </View>
+        <View style={{flexDirection:"row", justifyContent:"space-between", marginTop:10}}>
+            <Text style={{backgroundColor:"#f5f5f5",borderRadius:10,  color:"orange", padding:8}}>All</Text>
+            <Text style={{backgroundColor:"#f5f5f5",borderRadius:10, padding:8}}>Roadbike</Text>
+            <Text style={{backgroundColor:"#f5f5f5",borderRadius:10, padding:8 }}>Mountain</Text>
+            {/*<Text style={{backgroundColor:"grey",borderRadius:10, width:50}}>Urban</Text>*/}
+        </View>
+        <View style={{ flex: 1, }}>
+            <FlatList 
+            
+            numColumns={2}
+            keyExtractor = {(item) => item.id}
+
+              data={images}
+              renderItem={({ item }) =>(
+                  <View style={{padding: 25, backgroundColor: '#e9e8ed', margin: 10, borderRadius: 20, marginTop:20,}}>
+                  <View style={{alignItems:"flex-end",}}>
+                      <FontAwesome name="heart-o" size={24} style={{  }} />
+                  </View>
+
+                  <Image 
+                  style={{ height: 100, width:100 }}
+                   source={item.image} />
+                   <Text>{item.name}</Text>
+                   <Text style={{fontWeight:"bold"}}>{item.price}</Text>
+
+                  </View>
+
+              )}
+            />
+
+        </View>
+        
+        <View style={{backgroundColor:"#e9e8ed", flexDirection:"row", justifyContent:"space-between"}}>
+            <TouchableOpacity 
+            onPress={() => {
+                navigation.navigate("login")
+            }}>
+            <Foundation name="home" size={24} style={{color:"orange"}} />
+          </TouchableOpacity>
+
+            <View style={{height:40, width:40, borderRadius:20, backgroundColor:"white",transform:[{translateY:-20}]}}>
+            <View style={{height:40, width:40, borderRadius:20, backgroundColor:"black",}}>
+            <MaterialIcons name="keyboard-voice" size={24} color="white" style={{paddingLeft:7}} />
+            </View>
+            </View>
+
+            <FontAwesome5 name="shopping-bag" size={24} style={{color:"grey"}} />
+            
+            
+        </View>
+    </View>
+}
---
+++ login.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, Image} from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { AntDesign } from '@expo/vector-icons';
+
+
+export default function login({navigation}) {
+    return <View style={{flex:1, justifyContent:"center",
+    alignItems:"center", backgroundColor:"white"
+    }}>
+        <Image 
+        style={{
+            width:250,
+            height:170, 
+            borderRadius:20,
+            marginBottom:20,
+            transform:[{rotate:"-15deg"}]
+        }}
+        source = {{  uri:"https://surlybikes.com/uploads/bikes/Wednesday_BK0052_Background-1200x800.jpg"}}
+         />
+        <Text style={{color:"rgba(0,0,0,0.6)", fontSize:24}}>
+            Welcome to 
+        </Text>
+        <Text style={{color:"black", fontSize:30, fontWeight:600}}>
+            Power Bike Shop
+        </Text>
+        <TouchableOpacity 
+        onPress={() => {
+            navigation.navigate("home")
+        }}
+        style={{backgroundColor:"#e3e3e3",
+        padding:10,
+         paddingHorizontal:60,
+        marginTop:20,
+        flexDirection:"row",
+        borderRadius:10
+        }}> 
+        <AntDesign name="google" size={24} color="black" />
+        <Text style={{fontSize:20, paddingLeft:5}}>Login with Gmail</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+        style={{backgroundColor:"black",
+        borderRadius:10,
+        padding:10,
+        paddingHorizontal:60,
+        flexDirection:"row",
+        marginTop:20}}>
+            <AntDesign name="apple1" size={20} color="white" />
+            <Text style={{fontSize:20, color:"white", paddingLeft:5}}>Login with Apple</Text>
+
+        </TouchableOpacity>
+        <Text style={{marginTop:10, fontWeight:'500', color:'grey'}}>Not a member?
+         <Text style={{color:'orange', }}>Signup</Text>
+        </Text>
+         
+
+    </View>;
+}
